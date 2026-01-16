be028479f0015258ae83259bf91e22bab4674f71
rhinci <dashalupanova@gmail.com>
2026-01-16 23:05:08 +1000

    cors для интеграции с фронтендом

--- lab2/settings.py
+++ lab2/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_filters',
     'blog.apps.BlogConfig',
     'rest_framework',
+    'corsheaders', 
 ]
 
 REST_FRAMEWORK = {
@@ -77,6 +78,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -164,3 +166,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", 
+]
